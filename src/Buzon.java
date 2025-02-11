c789b1264bd803fa799b4cb4626517251c84cc89
María Sanabria <ma.sanabrias@uniandes.edu.co>
2025-02-11 18:33:13 -0500

    Clase Buzon

--- src/Buzon.java
+++ src/Buzon.java
@@ -1,6 +1,37 @@
+import java.util.LinkedList;
 import java.util.Queue;
 
 public class Buzon {
     private Queue<Producto> productos;
     private int capacidad;
+     
+    public Buzon(int capacidad) {
+        this.capacidad = capacidad;
+        this.productos = new LinkedList<>();
+    }
+
+    public synchronized void depositar(Producto p) throws InterruptedException {
+        while (productos.size() >= capacidad) {
+            wait(); 
+        }
+        productos.add(p);
+        notifyAll(); 
+    }
+    
+    public synchronized Producto retirar() throws InterruptedException {
+        while (productos.isEmpty()) {
+            wait(); 
+        }
+        Producto p = productos.poll();
+        notifyAll(); // Notifica a los hilos en espera
+        return p;
+    }
+
+    public synchronized boolean estaVacio() {
+        return productos.isEmpty();
+    }
+
+    public synchronized boolean estaLleno() {
+        return productos.size() >= capacidad;
+    }
 }
