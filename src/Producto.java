518e12c06bfd20d096f3944335fb237fda9b5e7f
María Sanabria <ma.sanabrias@uniandes.edu.co>
2025-02-11 18:40:12 -0500

    Producto

--- src/Producto.java
+++ src/Producto.java
@@ -2,5 +2,27 @@
 public class Producto {
     private int id;
     private boolean aprobado;
-   
+
+        public Producto(int id) {
+            this.id = id;
+            this.aprobado = false; // Por defecto, no aprobado
+        }
+    
+        public int getId() {
+            return id;
+        }
+    
+        public boolean isAprobado() {
+            return aprobado;
+        }
+    
+        public void setAprobado(boolean aprobado) {
+            this.aprobado = aprobado;
+        }
+    
+        @Override
+        public String toString() {
+            return "Producto " + id + " [" + (aprobado ? "Aprobado" : "Rechazado") + "]";
+        }
+    
 }
